40479ab0de088f06501562ec15fe2a731799a3d1
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-01-22 15:49:06 -0500

    refactor: Product Photo Carousel

--- src/app/components/ProductPhotoCarousel/ProductPhotoCarousel.tsx
+++ src/app/components/ProductPhotoCarousel/ProductPhotoCarousel.tsx
@@ -1,75 +1,89 @@
 "use client";
-import { Navigation, Pagination, Scrollbar, A11y, Thumbs } from "swiper/modules";
-import { Swiper, SwiperClass, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import { Box } from "@mui/material";
+import { useState } from "react";
+
+// Styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/thumbs";
-import { Box } from "@mui/material";
-import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    color: "blue",
+  },
+  {
+    id: 2,
+    color: "green",
+  },
+  {
+    id: 3,
+    color: "red",
+  },
+  {
+    id: 4,
+    color: "violet",
+  },
+];
+
+const Slides = images.map((image) => (
+  <SwiperSlide key={image.id}>
+    <Box
+      width={"100%"}
+      height={"400px"}
+      sx={{ backgroundColor: `${image.color}`, borderRadius: 1 }}
+    ></Box>
+  </SwiperSlide>
+));
+
+interface ThumbnailsProps {
+  selectedIndex: number | null;
+  onChange: (index: number) => void;
+}
+
+function Thumbnails({ selectedIndex, onChange }: ThumbnailsProps) {
+  return images.map((image, index) => (
+    <Box
+      component={"div"}
+      key={image.id}
+      width={"100%"}
+      height={"100px"}
+      sx={{
+        backgroundColor: `${image.color}`,
+        borderRadius: 1,
+        border: selectedIndex != null && selectedIndex == index ? "1px solid white" : "none",
+      }}
+      onClick={() => onChange(index)}
+    ></Box>
+  ));
+}
 
 export default function ProductPhotoCarousel() {
   const [swiper, setSwiper] = useState<SwiperClass | null>(null);
-  const [swiperIndex, setSwiperIndex] = useState<number>(0);
+  const [swiperIndex, setSwiperIndex] = useState<number | null>(null);
 
-  const images = [
-    {
-      id: 1,
-      color: "blue",
-    },
-    {
-      id: 2,
-      color: "green",
-    },
-    {
-      id: 3,
-      color: "red",
-    },
-    {
-      id: 4,
-      color: "violet",
-    },
-  ];
+  function handleChangeIndex(index: number) {
+    swiper && swiper.slideTo(index);
+    setSwiperIndex(index);
+  }
+
+  // * This functions it executes when swiper carousel mounts
+  // * it receives a swiper object that controls all the carousel
+  function handleSwiper(swiper: SwiperClass) {
+    setSwiper(swiper);
+  }
 
   return (
     <Box padding={1} flexGrow={1} maxWidth={{ sx: "100%", md: "40%" }} width={"100%"}>
-      <Swiper
-        onSwiper={(swiper) => {
-          setSwiper(swiper);
-        }}
-        modules={[Navigation, Pagination, Scrollbar, A11y, Thumbs]}
-        spaceBetween={50}
-        slidesPerView={1}
-      >
-        {images.map((image) => (
-          <SwiperSlide key={image.id}>
-            <Box
-              width={"100%"}
-              height={"400px"}
-              sx={{ backgroundColor: `${image.color}`, borderRadius: 1 }}
-            ></Box>
-          </SwiperSlide>
-        ))}
+      <Swiper onSwiper={handleSwiper} modules={[Navigation]} spaceBetween={50} slidesPerView={1}>
+        {Slides}
       </Swiper>
       <Box display={"flex"} gap={2} mt={2}>
-        {images.map((image, index) => (
-          <Box
-            component={"div"}
-            key={image.id}
-            width={"100%"}
-            height={"100px"}
-            sx={{
-              backgroundColor: `${image.color}`,
-              borderRadius: 1,
-              border: swiperIndex == index ? "1px solid white" : "none",
-            }}
-            onClick={() => {
-              swiper?.slideTo(index);
-              setSwiperIndex(index);
-            }}
-          ></Box>
-        ))}
+        <Thumbnails onChange={handleChangeIndex} selectedIndex={swiperIndex} />
       </Box>
     </Box>
   );
