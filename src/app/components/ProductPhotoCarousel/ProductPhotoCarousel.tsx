0c5a6fcd2840a60c1519ccd75b7be26d8bb993b4
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-01-22 15:26:28 -0500

    feat: Add Product Carousel

--- src/app/components/ProductPhotoCarousel/ProductPhotoCarousel.tsx
+++ src/app/components/ProductPhotoCarousel/ProductPhotoCarousel.tsx
@@ -1,38 +1,76 @@
-import Box from "@mui/material/Box";
-import { Carousel } from "react-responsive-carousel";
-import Image from "next/image";
-import { Typography } from "@mui/material";
+"use client";
+import { Navigation, Pagination, Scrollbar, A11y, Thumbs } from "swiper/modules";
+import { Swiper, SwiperClass, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/thumbs";
+import { Box } from "@mui/material";
+import { useState } from "react";
 
 export default function ProductPhotoCarousel() {
+  const [swiper, setSwiper] = useState<SwiperClass | null>(null);
+  const [swiperIndex, setSwiperIndex] = useState<number>(0);
+
+  const images = [
+    {
+      id: 1,
+      color: "blue",
+    },
+    {
+      id: 2,
+      color: "green",
+    },
+    {
+      id: 3,
+      color: "red",
+    },
+    {
+      id: 4,
+      color: "violet",
+    },
+  ];
+
   return (
-    <Carousel>
-      <Box>
-        <Image
-          src="https://th.bing.com/th/id/OIP.HP55nAQfHY4mlb4v9MxJKAHaEK?rs=1&pid=ImgDetMain"
-          alt="Imagen"
-          width={800}
-          height={600}
-        />
-        <Typography>sdfsdfsf</Typography>
+    <Box padding={1} flexGrow={1} maxWidth={{ sx: "100%", md: "40%" }} width={"100%"}>
+      <Swiper
+        onSwiper={(swiper) => {
+          setSwiper(swiper);
+        }}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Thumbs]}
+        spaceBetween={50}
+        slidesPerView={1}
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image.id}>
+            <Box
+              width={"100%"}
+              height={"400px"}
+              sx={{ backgroundColor: `${image.color}`, borderRadius: 1 }}
+            ></Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Box display={"flex"} gap={2} mt={2}>
+        {images.map((image, index) => (
+          <Box
+            component={"div"}
+            key={image.id}
+            width={"100%"}
+            height={"100px"}
+            sx={{
+              backgroundColor: `${image.color}`,
+              borderRadius: 1,
+              border: swiperIndex == index ? "1px solid white" : "none",
+            }}
+            onClick={() => {
+              swiper?.slideTo(index);
+              setSwiperIndex(index);
+            }}
+          ></Box>
+        ))}
       </Box>
-      <Box>
-        <Image
-          src="https://th.bing.com/th/id/OIP.HP55nAQfHY4mlb4v9MxJKAHaEK?rs=1&pid=ImgDetMain"
-          alt="Imagen"
-          width={800}
-          height={600}
-        />
-        <Typography>sdfsdfsf</Typography>
-      </Box>
-      <Box>
-        <Image
-          src="https://th.bing.com/th/id/OIP.HP55nAQfHY4mlb4v9MxJKAHaEK?rs=1&pid=ImgDetMain"
-          alt="Imagen"
-          width={800}
-          height={600}
-        />
-        <Typography>sdfsdfsf</Typography>
-      </Box>
-    </Carousel>
+    </Box>
   );
 }
