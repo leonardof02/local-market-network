b35fbb5a7bc693dbb16247e6634e2f86e1e46609
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-01-22 16:51:34 -0500

    feat: Close Session Button to Profile Page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -56,6 +56,9 @@ export default function ProfilePage() {
             <Typography>Guardar</Typography>
           </Box>
         </Button>
+        <Button fullWidth variant="contained" color="error">
+          Cerrar sesion
+        </Button>
       </Paper>
     </Box>
   );
