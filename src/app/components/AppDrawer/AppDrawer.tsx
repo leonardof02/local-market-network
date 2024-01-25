96b533934b91c962c80d9646ed0b8fcbc6bccf52
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-01-25 15:38:57 -0500

    refactor: App Drawer Menu Items

--- src/app/components/AppDrawer/AppDrawer.tsx
+++ src/app/components/AppDrawer/AppDrawer.tsx
@@ -3,23 +3,50 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+
+import { Home, Shop, Dashboard, Contacts } from "@mui/icons-material";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const menuItems = [
+  {
+    text: "Home",
+    icon: <Home />,
+  },
+  {
+    text: "Ver Productos",
+    icon: <Shop />,
+  },
+  {
+    text: "Tus Productos en Venta",
+    icon: <Dashboard />,
+  },
+  {
+    text: "Profile",
+    icon: <Contacts />,
+  },
+];
+
+function ListItems() {
+  return menuItems.map((item, index) => (
+    <ListItem key={item.text}>
+      <ListItemButton>
+        <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItemText primary={item.text} />
+      </ListItemButton>
+    </ListItem>
+  ));
+}
+
 export default function AppDrawer({ isOpen, onClose }: Props) {
   return (
     <Drawer open={isOpen} onClose={onClose}>
-      <List sx={{padding: 3}}>
-        {["Home", "Ver Productos", "Perfil", "Dashboard"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+      <List>
+        <ListItems />
       </List>
     </Drawer>
   );
